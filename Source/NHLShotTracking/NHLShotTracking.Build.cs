2f9a64af581f6d8bb0933000b88ae47f0e3b1c8d
Your Name <tmrr86@mun.ca>
2025-08-21 18:29:56 -0230

    build: Add RenderCore, RHI, and Projects to runtime modules

--- Source/NHLShotTracking/NHLShotTracking.Build.cs
+++ Source/NHLShotTracking/NHLShotTracking.Build.cs
@@ -8,7 +8,7 @@ public class NHLShotTracking : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "RenderCore", "RHI", "Projects" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
