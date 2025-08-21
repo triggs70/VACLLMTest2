6321665fdb49b605f42367568f18c3a410c148c4
Your Name <tmrr86@mun.ca>
2025-08-21 17:53:30 -0230

    feat: Add UShotDataLoader class

---
+++ Source/NHLShotTracking/ShotDataLoader.cpp
@@ -0,0 +1,1 @@
+#include "ShotDataLoader.h"
---
+++ Source/NHLShotTracking/ShotDataLoader.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "ShotDataLoader.generated.h"
+
+UCLASS()
+class NHLSHOTTRACKING_API UShotDataLoader : public UObject
+{
+    GENERATED_BODY()
+};
